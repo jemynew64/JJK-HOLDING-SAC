939a786bbfccbabbc6bb5092d28fcd342a29bbba
jemal leon <jemalxd8@gmail.com>
2024-12-22 12:51:10 -0500

    footer-v1 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
-
+import { Carousel } from "./components/Carousel";
+import { Navbar } from "./components/Navbar";
 function App() {
   return (
     <>
-      <p>hola gente</p>
+      <Navbar />
+      <Carousel />
+      <h1>acerca de </h1>
+      <h1>servicios destacados</h1>
+      <h1>principales clientes</h1>
+      <h1>contacto</h1>
+      <h1>footer</h1>
     </>
   );
 }
